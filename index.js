c0aafb9ee38207259528c2afaca1c15bd5f6c60e
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-04-01 14:46:21 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// declarion of the variable in global scoop
+var customerName = 'bob';
+const leastFavouriteCustomer= new Customer
+const changeleastFavouriteCustomer =  Customer
+// upperCaseCustomerName(): Write a function that accesses that global customerName variable, and uppercases it.
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  
+  function setBestCustomer() {
+    var bestCustomer = 'not bob'
+    return bestCustomer;
+  
+    function overwriteCustomer(){
+        var bestCustomer = 'maybe bob'
+        return bestCustomer;
+    }
+
+    function changeleastFavouriteCustomer() {
+        const leastFavouriteCustomer = 'not bob'
+        return leastFavouriteCustomer;
+    
+}
+
+}
+}
